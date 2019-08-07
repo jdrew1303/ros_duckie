fff83c5c2ec3cd1cd28d42a926eb667f75690e89
James Drew <j.drew1303@gmail.com>
2019-08-07 18:24:00 +0100

    cleaning up processes

--- run_all_nodes.sh
+++ run_all_nodes.sh
@@ -14,9 +14,11 @@ roscore &
 sleep 10
 
 # start up our services in the background (easier than a launch file)
-rosrun camera camera_node &
-rosrun differential differential_drive_node &
 rosrun cv_lane_follower lane_follower_node & 
+rosrun differential differential_drive_node &
+sleep 5
+rosrun camera camera_node &
+
 
 while sleep 60; do
   rostopic list
